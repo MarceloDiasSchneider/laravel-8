20aef22c7161636091620e48f377d8d7cb6f8e59
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-03-09 11:40:05 +0100

    Rest API
    Rest Api Created
    API to sellmaster_frontend get json from links
    Api Game Resource

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Post;
+use App\Http\Controllers\PostApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ScriptPerGiornataController;
+use App\Http\Controllers\OrdiniPerGiornoMondoTopController;
+use App\Http\Controllers\OrdiniPerOraMondoTopController;
+
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +25,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostApiController::class, 'get']);
+Route::get('/posts/{getId}', [PostApiController::class, 'getId']);
+Route::post('/posts', [PostApiController::class, 'post']);
+Route::put('/posts/{idToUpdate}', [PostApiController::class, 'update']);
+Route::delete('/posts/{idToDelete}', [PostApiController::class, 'delete']); 
+
+Route::get('scriptPerGiornata',[ScriptPerGiornataController::class,'scriptPerGiornata']);
+Route::get('ordiniPerGiornoMondoTop', [OrdiniPerGiornoMondoTopController::class, 'ordiniPerGiornoMondoTop']);
+Route::get('ordiniPerOraMondoTop', [OrdiniPerOraMondoTopController::class, 'ordiniPerOraMondoTop']);
+
+Route::apiResource("game", GameController::class);
